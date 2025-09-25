520f77d826015f93610b6a5d207404d151ac82f0
J-F Bilodeau <jfbilodeau@chronogears.com>
2025-09-25 11:21:26 -0400

    Add unit tests for Class1 and update project references

--- ProjectDelorean.Test/Class1.cs
+++ ProjectDelorean.Test/Class1.cs
@@ -1,6 +1,29 @@
-﻿namespace ProjectDelorean.Test;
+﻿
+using Xunit;
+using ProjectDelorean.Lib;
 
-public class Class1
+namespace ProjectDelorean.Test
 {
+    public class Class1Tests
+    {
+        [Fact]
+        public void SampleTest_ShouldPass()
+        {
+            // Arrange
+            var obj = new Class1();
 
+            // Act & Assert
+            Assert.NotNull(obj);
+        }
+
+        [Fact]
+        public void AnotherTest_ShouldFail()
+        {
+            // Arrange
+            var obj = new Class1();
+
+            // Act & Assert
+            Assert.IsType<string>(obj);
+        }
+	}
 }
